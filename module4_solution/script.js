3f87e2dc517a6f402b2199aa2f93bc1ee3b30b1d
AntonMFedak <97338191+AntonMFedak@users.noreply.github.com>
2022-02-08 14:43:41 +0000

    Update script.js

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,5 +1,5 @@
 // SOLUTION
-(function (window) {
+(function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
@@ -9,7 +9,7 @@
       helloSpeaker.speak(names[i]);
     }
   }
-})(window);
+})();
 
 
 // **** INSTRUCTIONS ****
